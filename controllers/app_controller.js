3828bd3759dbbbbe2a153b3afd51e2d57225b873
cookies4u <jcreyes.1018@gmail.com>
2017-06-13 19:22:07 -0700

    controller mongoose code updated

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -1,6 +1,5 @@
 // Dependencies
 var express = require("express");
-var mongoose = require("mongoose"); // may not need
 var app = express.Router();
 // Requiring our Note and Article models aka tables
 var Note = require("../models/Note.js");
@@ -8,8 +7,6 @@ var Article = require("../models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise; // may not need
 
 // Routes
 // ======
